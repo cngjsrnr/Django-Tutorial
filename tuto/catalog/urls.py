8a8143bf41bfc487877a92658494c7fb68d124cb
jaan <cngjsrnr@naver.com>
2020-08-06 14:30:01 +0900

    Add BookList, BookDetail, AuthorList, AuthorDetail
    6장의 책리스트 ,책상세보기, 저자리스트, 저자 상세보기 추가

--- tuto/catalog/urls.py
+++ tuto/catalog/urls.py
@@ -4,4 +4,9 @@ from catalog import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('book/', views.BookListView.as_view(), name='book'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='author'),
+    path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    
 ]
--- tuto/catalog/views.py
+++ tuto/catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 def index(request):
     """View function for home page of site."""
@@ -26,4 +27,52 @@ def index(request):
     }
 
     # Render the HTML template index.html with the data in the context variable
-    return render(request, ['index.html'], context=context)
+    return render(request, 'index.html', context=context)
+
+'''
+generic views는 /application_name/the_model_name_list.html의 경로를 가짐
+여기에서는 catalog/book_list.html.임
+근데 여기에서는 TEMPLATES경로를 template로 했으니
+경로가 /catalog/templates/catalog/book_list.html 이렇게됨
+'''
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+    '''
+    context_object_name = 'my_book_list'   # your own name for the list as a template variable  book_list.html에서 이걸로 이름 바꿈
+    
+    queryset = Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war 유저가 읽은순으로 5개의 책을 나열
+    #template_name = 'books/my_arbitrary_template_name_list.html'  # Specify your own template name/location
+    '''
+    '''
+    def get_queryset(self):
+        return Book.objects.filter(title__icontains='war')[:5] # Get 5 books containing the title war
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['some_data'] = 'This is just some data'
+        return context
+    '''
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    
+
+
+'''위에꺼를 함수기반으로 만들경우
+def book_detail_view(request, primary_key):
+    try:
+        book = Book.objects.get(pk=primary_key)
+    except Book.DoesNotExist:#오류처리를 직접해줘야함
+        raise Http404('Book does not exist')
+    
+    return render(request, 'catalog/book_detail.html', context={'book': book})
+'''
+
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
